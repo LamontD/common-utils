752a8dfbc9a1a130986d7487cbcc43d3e4a72ba5
LamontD <lamont.dozier.jr@gmail.com>
2020-05-09 14:22:57 -0400

    Initial commit

---
+++ src/main/java/com/lamontd/utils/jackson/JacksonMapper.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 lamontdozierjr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+  */
+package com.lamontd.utils.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * Singleton for getting a Jackson JSON mapper that looks the way I want it to.
+ * @author lamontdozierjr
+ */
+public class JacksonMapper {
+
+    private static ObjectMapper jacksonMapper;
+    private static ObjectMapper prettyMapper;
+
+    private JacksonMapper() {
+    }
+
+    public static final ObjectMapper getStandardMapper() {
+        if (jacksonMapper == null) {
+            jacksonMapper = new ObjectMapper();
+            jacksonMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            jacksonMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+            jacksonMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        }
+        return jacksonMapper;
+    }
+
+    public static final ObjectMapper getPrettyMapper() {
+        if (prettyMapper == null) {
+            prettyMapper = new ObjectMapper();
+            prettyMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            prettyMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+            prettyMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            prettyMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        }
+        return prettyMapper;
+    }
+
+}
---
+++ src/main/java/com/lamontd/utils/model/MappedTransportObject.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 lamontdozierjr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+  */
+package com.lamontd.utils.model;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+/**
+ * Object that uses reflection-based mapping to package an object for transporting. 
+ * @author lamontdozierjr
+ */
+public class MappedTransportObject {
+    private String objectType;
+    private String transactionId;
+    private HashMap<String, Object> attributes = new HashMap<>();
+    public MappedTransportObject() {}
+    public MappedTransportObject(Object subject) {
+        if (subject != null) {
+        this.objectType = subject.getClass().getName();
+        for(Field fld : FieldUtils.getAllFields(subject.getClass())) {
+            try {
+                attributes.put(fld.getName(), FieldUtils.readField(fld, subject, true));
+            } catch (IllegalAccessException ex) {}
+        }
+        }
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public HashMap<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(HashMap<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+    
+    
+}
